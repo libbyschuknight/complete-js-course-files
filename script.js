e77c711d30261f62bca4d1055b57924cac2b7631
libbyschuknight <libbysk@gmail.com>
2019-06-16 20:22:18 +1200

    My BMI attempt

--- script.js
+++ script.js
@@ -28,22 +28,56 @@ var joanMary = "";
 Variable mutation and type coercion
 */
 
-// Type coercion
-var firstName = 'Joan';
-var age = 28;
-console.log(firstName + ' ' + age);
+// // Type coercion
+// var firstName = 'Joan';
+// var age = 28;
+// console.log(firstName + ' ' + age);
 
-var job, isMarried;
-job = 'Teacher';
-isMarried = false;
+// var job, isMarried;
+// job = 'Teacher';
+// isMarried = false;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '.Is she married? ' + isMarried);
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '.Is she married? ' + isMarried);
 
-// Variable mutation
-age = 'twenty eight';
-job = 'driver';
+// // Variable mutation
+// age = 'twenty eight';
+// job = 'driver';
 
-alert(firstName + ' is a ' + age + ' year old ' + job + '.Is she married? ' + isMarried);
+// alert(firstName + ' is a ' + age + ' year old ' + job + '.Is she married? ' + isMarried);
 
-var lastName = prompt('What is his last name?');
-console.log(firstName + ' ' + lastName);
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + ' ' + lastName);
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true")
+*/
+
+// store mass and height in vars
+var marksMass = 80;
+var marksHeight = 1.65;
+
+var johnsMass = 120;
+var johnsHeight = 1.77;
+
+// caluate both BMIS
+// var marksBmi = marksMass / (marksHeight * marksHeight);
+// var johnsBmi = johnsMass / (johnsHeight * johnsHeight);
+
+function bmi(mass, height) {
+  return mass / (height * height);
+}
+
+var marksBmi = bmi(marksMass, marksHeight);
+var johnsBmi = bmi(johnsMass, johnsHeight);
+
+console.log(marksBmi, johnsBmi);
+
+// boolean for is marks bmi higher then johns
+var isMarksBmiHigher = marksBmi > johnsBmi;
+
+// print to console
+console.log('Is Marks BMI higher than Johns?', isMarksBmiHigher);
