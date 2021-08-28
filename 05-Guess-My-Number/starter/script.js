922c9fc8528c2790130cee3f386818f9191d93d2
libbyschuknight <libbysk@gmail.com>
2021-08-28 18:18:20 +1200

    start of DOM stuff

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct number';
+
+const number = (document.querySelector('.number').textContent = 13);
+const score = (document.querySelector('.score').textContent = 11);
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 33;
+console.log(document.querySelector('.guess').value);
