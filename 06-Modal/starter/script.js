8b3f47759ed443dedd1a98c11826ff06a2c0143f
libbyschuknight <libbysk@gmail.com>
2021-09-05 18:00:55 +1200

    Modal window project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// btnsOpenModal.forEach(element => {
+//   console.log(element.textContent);
+// });
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let index = 0; index < btnsOpenModal.length; index++) {
+  btnsOpenModal[index].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
